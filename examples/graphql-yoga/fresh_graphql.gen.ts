e95bc65957f689ed6f7d7a09eb3bda3d5cd6ccce
Vicary A <vicary.archangel@member.mensa.org>
2022-10-11 00:51:29 +0800

    fix(example/yoga): typo

--- examples/graphql-yoga/fresh_graphql.gen.ts
+++ examples/graphql-yoga/fresh_graphql.gen.ts
@@ -3,10 +3,12 @@
 // This file should be checked into source control.
 
 import * as $0 from "./graphql/Query/foo/mod.ts";
+import * as $1 from "./graphql/Subscription/countdown/mod.ts";
 
 const manifest = {
   modules: {
     "Query/foo/mod.ts": $0,
+    "Subscription/countdown/mod.ts": $1,
   },
   baseUrl: import.meta.url,
 };
---
+++ examples/graphql-yoga/graphql/Subscription/countdown/mod.ts
@@ -0,0 +1,15 @@
+import type { IFieldResolver } from "@graphql-tools/utils";
+
+export const schema = /* GraphQL */ `
+  extend type Subscription {
+    countdown(from: Int!): Int!
+  }
+`;
+
+export const resolver: IFieldResolver<unknown, unknown, { from: number }> =
+  async function* (_, { from }) {
+    for (let i = from; i >= 0; i--) {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      yield { countdown: i };
+    }
+  };
