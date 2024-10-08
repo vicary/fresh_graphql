e35fc3b61a84a80a01af2123b4f08cb29504d0e3
Vicary A <vicary.archangel@member.mensa.org>
2024-10-08 17:13:18 +0800

    fix: stubbed type FreshContext

--- server.ts
+++ server.ts
@@ -9,16 +9,54 @@ export type CreateHandlerOptions = {
 /**
  * A stubbed version of FreshContext.
  *
- * Fresh 1.x uses HTTP import which is not compatible with JSR modules, to be
- * updated when Fresh 2.x is released.
+ * Fresh 1.x uses HTTP import which is not compatible with JSR modules, this
+ * will be a direct export from Fresh when 2.x is out.
  *
  * ```ts
  * import type { FreshContext } from "jsr:@fresh/core@^2.0";
  * ```
  */
-export type FreshContext = {
-  request: Request;
-};
+export interface FreshContext<
+  State = Record<string, unknown>,
+  // deno-lint-ignore no-explicit-any
+  Data = any,
+  NotFoundData = Data,
+> {
+  /** @types deprecated */
+  localAddr?: Deno.NetAddr;
+  remoteAddr: Deno.NetAddr;
+  url: URL;
+  basePath: string;
+  route: string;
+  /** @deprecated Use `.route` instead */
+  pattern: string;
+  destination: "internal" | "static" | "route" | "notFound";
+  params: Record<string, string>;
+  isPartial: boolean;
+  state: State;
+  config: {
+    dev: boolean;
+    build: {
+      outDir: string;
+      target: string | string[];
+    };
+    staticDir: string;
+    server: Partial<Deno.ServeTlsOptions>;
+    basePath: string;
+  };
+  data: Data;
+  /** The error that caused the error page to be loaded. */
+  error?: unknown;
+  /** Sringified code frame of the error rendering failed (only in development mode) */
+  codeFrame?: unknown;
+
+  // These properties may be different
+  renderNotFound: (data?: NotFoundData) => Response | Promise<Response>;
+  render: (
+    data?: Data,
+  ) => Response | Promise<Response>;
+  next: () => Promise<Response>;
+}
 
 export function createHandler<TManifest extends Manifest>(
   manifest: TManifest,
